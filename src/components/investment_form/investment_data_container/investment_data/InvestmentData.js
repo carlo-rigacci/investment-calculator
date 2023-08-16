ad4faa3fb5fb51b1f07108dc52551427d976fd3a
faint <faintcsr@gmail.com>
2023-08-16 17:05:55 +0200

    Added state to InvestmentData component

--- src/components/investment_form/investment_data_container/investment_data/InvestmentData.js
+++ src/components/investment_form/investment_data_container/investment_data/InvestmentData.js
@@ -1,10 +1,16 @@
 import './InvestmentData.css';
+import { useState } from 'react';
 
 export default function InvestmentData({ name, id }) {
+  const [inputState, setInputState] = useState('');
+  const onChangeHandler = ({ target: { value } }) => {
+    setInputState(value);
+  };
+
   return (
     <p>
       <label htmlFor={id}>{name}</label>
-      <input type='number' id={id} />
+      <input onChange={onChangeHandler} type='number' id={id} />
     </p>
   );
 }
