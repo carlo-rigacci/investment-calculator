d014e9dc6d40c11780ee32aac3edc0dc52225c20
faint <faintcsr@gmail.com>
2023-08-16 13:45:34 +0200

    Created InvestmentDataContainer

--- src/components/investment_form/InvestmentForm.js
+++ src/components/investment_form/InvestmentForm.js
@@ -1,9 +1,11 @@
 import './InvestmentForm.css';
+import InvestmentDataContainer from './investment_data_container/InvestmentDataContainer';
 
 export default function InvestmentForm() {
   return (
   <form className='form'>
-    
+    <InvestmentDataContainer/>
+    <InvestmentDataContainer/>
   </form>
   );
 }
---
+++ src/components/investment_form/investment_data_container/InvestmentDataContainer.js
@@ -0,0 +1,11 @@
+import './InvestmentDataContainer.css';
+import InvestmentData from './investment_data/InvestmentData'
+
+export default function InvestmentDataContainer() {
+  return (
+    <div className='input-group'>
+      <InvestmentData />
+      <InvestmentData />
+    </div>
+  );
+}
